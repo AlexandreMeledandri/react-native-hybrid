e4e6c933cc9ca5ac6512a3c078274b743bb60edc
Kazzbo <cedricaadam@gmail.com>
2018-09-18 16:15:21 -0700

    hum hum le vrais ...

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Jeu from './Components/Jeu';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TextInput style={styles.textinput} placeholder='Entrez votre valeur'/> 
-        <Button style={{heght}} title='Envoyer'onPress={() => {}}/>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <Jeu/>
       </View>
     );
   }
