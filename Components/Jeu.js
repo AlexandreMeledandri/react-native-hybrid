d59021c8726c510e8e4ab554ceab52160cf29148
AlexandreMeledandri <alexandre.meledandri@gmail.com>
2018-09-18 16:47:36 +0200

    first playable version

--- Components/Jeu.js
+++ Components/Jeu.js
@@ -1,15 +1,107 @@
 import React from 'react'
-import { View, TextInput, Button } from 'react-native'
+import { Text, StyleSheet, View, TextInput, Button } from 'react-native'
 
 class Jeu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          bodyText: 'Essayez de deviner le chiffre mystère. il est compris entre 1 et 1000.\n\nAttention vous n\'avez que 5 essais',
+          suggestedNumber: '',
+          number : Math.round(Math.random()*1000),
+          plays: 0
+        };
+      }
     render() {
         return (
             <View>
-            <TextInput placeholder='Titre du film'/>
-            <Button title='Rechercher' onPress={() => {
+                <Text style={styles.counter}>
+                Coups: {this.state.plays}
+            </Text>
+            <Text style={styles.bodyText}>
+                {this.state.bodyText}
+            </Text>
+            <TextInput 
+            style={styles.suggestInput} placeholder='Entrez un chiffre'
+            onChangeText={(suggestedNumber) => this.setState({suggestedNumber})}
+            value={this.state.suggestedNumber}/>
+            <View style={styles.marginBottom}>
+            <Button
+            title='Deviner' 
+            onPress={() => {
+                if(this.state.plays < 5) {
+                    this.hint(this.comparate());
+                    this.state.plays++;
+                } else {
+                    this.setState({
+                        bodyText: 'Vous n\'avez plus d\'essais'
+                    })
+                }
+            }}/>
+            </View>
+            <Button
+            title='Recommencer' 
+            onPress={() => {
+                this.reset();
             }}/>
             </View>
         )
     }
+    hint = (_value) => {
+        if(_value === 2){
+            this.setState({
+                        bodyText: 'Vous devez entrez un chiffre'
+                    })
+        }
+        else if(_value === 1) {
+            this.setState({
+                bodyText: 'c\'est plus',
+            })
+        } else if(_value === -1) {
+            this.setState({
+                bodyText: 'c\'est moins',
+            })
+        } else if(_value === 0) {
+            this.setState({
+                bodyText: 'Vous avez gagné',
+            })
+        }
+    }
+    comparate = () => {
+        if(this.state.suggestedNumber === '') {
+            return 2;
+        }
+        if(this.state.suggestedNumber.toString() > this.state.number) {
+            return -1;
+        } else if(this.state.suggestedNumber.toString() < this.state.number){
+            return 1;
+        } else {
+            return 0
+        }
+
+    }
+    reset = () => {
+        this.setState({
+            bodyText: 'Essayez de deviner le chiffre mystère. il est compris entre 1 et 1000.\n\nAttention vous n\'avez que 5 essais',
+            suggestedNumber: '',
+            number : Math.round(Math.random()*1000)
+          });
+          this.state.plays = 0;
+    }
 }
+const styles = StyleSheet.create({
+    suggestInput: {
+        width: 120,
+        padding:10,
+        marginBottom: 10
+    },
+    bodyText: {
+        width:140
+    },
+     marginBottom: {
+        marginBottom: 10
+    },
+    counter: {
+        marginBottom: 10
+    }
+  });
 export default Jeu
