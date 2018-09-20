ac091e3e7fa05637891b25ff8d02c315862d6198
AlexandreMeledandri <alexandre.meledandri@gmail.com>
2018-09-20 10:44:03 +0200

    added history.js

---
+++ Components/History.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+class Historique extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+            <Text style={styles.description}>Voici les résultats des 5 dernières parties :</Text>
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    description: {
+        width:140,
+    }
+})
+export default History
